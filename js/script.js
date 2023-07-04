7c1cb91a9bc2c315aa9015e9ff5942810272b9a3
MicheleGresta <malachia_1997@hotmail.it>
2023-07-04 16:05:55 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -1,18 +1,41 @@
 
+const totSquares = 10 * 10;
+const squareContainer = document.querySelector(".square-container");
 
 
+for (let i = 1; i <= totSquares; i++) {
+    let singleSquare = document.createElement("div");
+    singleSquare.classList.add("square-cell")
 
-
-
-let str = '';
-
-for (let i = 1; i <= 100; i++) {
+    
     
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz")
-    }   else if (i % 5 === 0) {
-        console.log("Buzz");
-    }   else if (i % 3 === 0) {
-        console.log("Fizz");
-    }   else (console.log(i))    
+        singleSquare.classList.add("bg-danger")
+        singleSquare.append("FizzBuzz")
+    } else if (i % 5 === 0) {
+        singleSquare.classList.add("bg-warning")
+        singleSquare.append("Buzz")
+    } else if (i % 3 === 0) {
+        singleSquare.classList.add("bg-success")
+        singleSquare.append("Fizz")
+    } else {
+        singleSquare.classList.add("bg-primary")
+        singleSquare.append(i)
+    }
+
+
+    squareContainer.append(singleSquare);
 }
+
+
+for (let i = 1; i <= totSquares; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else (console.log(i))
+}
+
